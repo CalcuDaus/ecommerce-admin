7fe1429908828660e955ab927531fe0f40bcd20d
daus <dausg878@gmail.com>
2024-01-19 15:08:36 +0700

    add setting page

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -4,6 +4,8 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -35,8 +37,16 @@ export const StoreModal = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-    // TODO: Create Store
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/stores", values);
+
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error("Somthing went wrong");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
