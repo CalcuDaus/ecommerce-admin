9597ea61915fe91aa3f88347592a0f592b44ab52
daus <dausg878@gmail.com>
2024-01-20 22:44:53 +0700

    image upload in coming

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  return <div>ImageUpload</div>;
+};
